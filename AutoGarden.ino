44a37866ec330596667933139abee234ed349d67
MyShillelagh <47699932+MyShillelagh@users.noreply.github.com>
2019-09-30 16:31:07 -0700

    Update AutoGarden.ino
    Fixed comments and added the final long delay back in

--- AutoGarden.ino
+++ AutoGarden.ino
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-int relayPins[] = { 5, 6, 7 };								//Pins used to activate the valves
-int pump = 8;												//Pin for water pump
-int sensorPins[] = { A0, A1, A2 };							//Pins used to measure moisture sensors
-int pinCount = 3;											//Number of valve-sensor pairs in the system
+int relayPins[] = { 5, 6, 7 };					//Pins used to activate the valves
+int pump = 8;							//Pin for water pump
+int sensorPins[] = { A0, A1, A2 };				//Pins used to measure moisture sensors
+int pinCount = 3;						//Number of valve-sensor pairs in the system
 
 void setup() {
 	for (int pinInit = 0; pinInit < pinCount; pinInit++)	//Loop to designate relay pins as outputs, done to help future scaling
@@ -16,16 +16,16 @@ void setup() {
 }
 
 void loop() {
-	for (int i = 0; i < pinCount; i++)						//Loop runs through the array of sensors set up previously
+	for (int i = 0; i < pinCount; i++)			//Loop runs through the array of sensors set up previously
 	{
-		Serial.print("Sensor ");							//Displays the current reading of each sensor in the serial monitor
+		Serial.print("Sensor ");			//Displays the current reading of each sensor in the serial monitor
 		Serial.print(i + 1);
 		Serial.print(": ");
 		Serial.println(analogRead(sensorPins[i]));
-		if (analogRead(sensorPins[i]) >= 400)				//Checks moisture of soil (higher = drier), range of 285-581
+		if (analogRead(sensorPins[i]) >= 400)		//Checks moisture of soil (higher = drier), range of 285-581
 		{
-			digitalWrite(relayPins[i], LOW);				//NOTE: the LOW and HIGH outputs may have to be switched depending
-			digitalWrite(pump, LOW);						//on how you wire the circuit together.
+			digitalWrite(relayPins[i], LOW);	//NOTE: the LOW and HIGH outputs may have to be switched depending
+			digitalWrite(pump, LOW);		//on how you wire the circuit together.
 			delay(2000);
 			digitalWrite(relayPins[i], HIGH);
 			digitalWrite(pump, HIGH);
@@ -38,5 +38,5 @@ void loop() {
 			delay(5000);
 		}
 	}
-	//delay(6*60*60*1000ul); 								//DON'T FORGET TO PUT THIS BACK TO NORMAL IN THE FINAL COPY
+	delay(6*60*60*1000ul);					//Delays next check for 6 hours
 }
